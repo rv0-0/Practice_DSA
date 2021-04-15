ef63ca4a2c78eead9e979182b69ce57deb313324
Ravi Kr. Singh <38377594+rv0-0@users.noreply.github.com>
2021-04-16 05:13:33 +0530

    Create 20. Reverse the Edges of Directed Graph (Transpose).java

---
+++ Graph/20. Reverse the Edges of Directed Graph (Transpose).java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static int floorPathLen = Integer.MAX_VALUE;
+    static String floorPath = "";
+
+    public static class Edge {
+        int v, wt;
+
+        Edge(int v, int wt) {
+            this.v = v;
+            this.wt = wt;
+        }
+        public String toString()
+        {
+            return v+"";
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<Integer, ArrayList<Edge>> g = new HashMap<>();
+        int n, e;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        e = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            g.put(i, new ArrayList<Edge>());
+        }
+
+        for (int i = 0; i < e; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            //int wt = sc.nextInt();
+            g.get(u).add(new Edge(v, 1));
+            //g.get(v).add(new Edge(u, 1));
+        }
+
+        HashMap<Integer, ArrayList<Edge>> transposedGraph = transpose(g,n);
+        System.out.println(g);
+        System.out.println(transposedGraph);
+
+    }
+
+    public static HashMap<Integer, ArrayList<Edge>> transpose(HashMap<Integer, ArrayList<Edge>> g , int n)
+    {
+        HashMap<Integer, ArrayList<Edge>> result = new HashMap<>();
+        for(int i=0;i<n;i++)
+            result.put(i, new ArrayList<>());
+
+        for(int i=0;i<n;i++)
+        {
+            for(Edge neigh : g.get(i))
+            {
+                result.get(neigh.v).add(new Edge(i,1));
+            }
+        }
+        return result;
+    }
+}
