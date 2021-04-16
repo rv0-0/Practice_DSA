3060374fc863a47e8778efc7ca53650096f541df
Ravi Kr. Singh <38377594+rv0-0@users.noreply.github.com>
2021-04-16 15:52:43 +0530

    Create Construction of Tree.java

---
+++ Generic Tree/Construction of Tree.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Main
+{
+    public static class Node
+    {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void main(String [] args)
+    {
+        Node root;
+
+        int [] arr={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
+
+        Stack<Node> st = new Stack<>();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i] == -1)
+                st.pop();
+            else
+            {
+                Node t = new Node();
+                t.data = arr[i];
+
+                if(st.size() == 0)
+                    root = t;
+                else
+                    st.peek().children.add(t);
+
+                st.push(t);
+            }
+        }
+    }
+}
