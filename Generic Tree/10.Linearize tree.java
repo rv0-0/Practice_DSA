d5eb3d98618e1a127b2a3c33e47a25d69dd7365e
Ravi Kr. Singh <38377594+rv0-0@users.noreply.github.com>
2021-04-16 17:42:01 +0530

    Update 10.Linearize tree.java

--- Generic Tree/10.Linearize tree.java
+++ Generic Tree/10.Linearize tree.java
@@ -1,3 +1,4 @@
+//Approch 1 O(n2)
 public static void linearize(Node node)
     {
         for(Node child: node.children)
@@ -16,4 +17,19 @@ public static void linearize(Node node)
         while(node.children.size()==1)
             node = node.children.get(0);
         return node;
+    }
+//Approch 2 O(n)
+public static Node linearize2(Node node)
+    {
+        if(node.children.size() == 0)
+            return node;
+        Node lkt = linearize2(node.children.get(node.children.size()-1));
+        while(node.children.size()>1)
+        {
+            Node last = node.children.remove(node.children.size()-1);
+            Node secondlast = node.children.get(node.children.size()-1);
+            Node secondlastTail  = linearize2(secondlast);
+            secondlastTail.children.add(last);
+        }
+        return lkt;
     }
