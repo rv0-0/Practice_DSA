b449b43166fd939e9692318f585f5a3131d4101d
Ravi Kr. Singh <38377594+rv0-0@users.noreply.github.com>
2021-04-16 18:46:53 +0530

    Create 19. Ceil and floor.java

---
+++ Generic Tree/19. Ceil and floor.java
@@ -0,0 +1,18 @@
+    static void ceilAndFloor(Node node, int data)
+    {
+        if(node.data>data)
+        {
+            if(node.data<ceil)
+                ceil = node.data;
+        }
+        
+        if(node.data<data)
+        {
+            if(node.data>floor)
+                floor=node.data;
+        }
+
+        for(Node child: node.children)
+            ceilAndFloor(child, data);
+    }
+}
