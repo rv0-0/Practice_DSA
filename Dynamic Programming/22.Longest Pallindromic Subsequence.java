abda0741c4306c308717f1b3d826f9ec3f4f6641
Ravi Kr. Singh <38377594+rv0-0@users.noreply.github.com>
2021-04-17 18:20:08 +0530

    Create 22.Longest Pallindromic Subsequence.java

---
+++ Dynamic Programming/22.Longest Pallindromic Subsequence.java
@@ -0,0 +1,30 @@
+public static int longestPallindromicSubsequence(String A)
+    {
+        String rev = "";
+        for(int i=0;i<A.length();i++)
+            rev = A.charAt(i) + rev;
+        return  longestCommonSubsequence(A,rev);
+    }
+    private static int longestCommonSubsequence(String A, String B)
+    {
+        int row = A.length()+1;
+        int col =B.length()+1;
+        int [][] dp = new int [row][col];
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j] = 0;
+                else
+                {
+                    if(A.charAt(i-1) == B.charAt(j-1))
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    else
+                        dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[row-1][col-1];
+
+    }
